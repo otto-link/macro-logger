e884ef005801df28502d4a59c230b044ad138bb2
Otto Link <otto.link.bv@gmail.com>
2023-11-10 23:26:57 +0100

    Remove objective C macros and modify calls to variadic macros (for windows portability)

--- macrologger.h
+++ macrologger.h
@@ -23,12 +23,8 @@
 #ifndef __MACROLOGGER_H__
 #define __MACROLOGGER_H__
 
-#ifdef __OBJC__
-#import <Foundation/Foundation.h>
-#else
 #include <time.h>
 #include <string.h>
-#endif
 
 // === auxiliar functions
 static inline char *timenow();
@@ -44,23 +40,6 @@ static inline char *timenow();
 #define LOG_LEVEL   DEBUG_LEVEL
 #endif
 
-#ifdef __OBJC__
-
-#if __has_feature(objc_arc)
-#define AUTORELEASEPOOL_BEGIN   @autoreleasepool {
-#define AUTORELEASEPOOL_END     }
-#define RELEASE(OBJ)            OBJ = nil
-#else
-#define AUTORELEASEPOOL_BEGIN   NSAutoreleasePool *_pool = [[NSAutoreleasePool alloc] init];
-#define AUTORELEASEPOOL_END     [_pool release];
-#define RELEASE(OBJ)            [OBJ release];
-#endif
-
-#define PRINTFUNCTION(format, ...)      objc_print(@format, __VA_ARGS__)
-#else
-#define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
-
-#endif
 
 #define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
 #define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
@@ -72,27 +51,27 @@ static inline char *timenow();
 #define DEBUG_TAG   "DEBUG"
 
 #if LOG_LEVEL >= DEBUG_LEVEL
-#define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
+#define LOG_DEBUG(message, ...)     fprintf(stderr, LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## __VA_ARGS__)
 #else
-#define LOG_DEBUG(message, args...)
+#define LOG_DEBUG(message, ...)
 #endif
 
 #if LOG_LEVEL >= INFO_LEVEL
-#define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
+#define LOG_INFO(message, ...)   fprintf(stderr, LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## __VA_ARGS__)
 #else
-#define LOG_INFO(message, args...)
+#define LOG_INFO(message, ...)
 #endif
 
 #if LOG_LEVEL >= ERROR_LEVEL
-#define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#define LOG_ERROR(message, ...)   fprintf(stderr, LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## __VA_ARGS__)
 #else
-#define LOG_ERROR(message, args...)
+#define LOG_ERROR(message, ...)
 #endif
 
 #if LOG_LEVEL >= NO_LOGS
-#define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#define LOG_IF_ERROR(condition, message, ...) if (condition) fprintf(stderr, LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## __VA_ARGS__)
 #else
-#define LOG_IF_ERROR(condition, message, args...)
+#define LOG_IF_ERROR(condition, message, ...)
 #endif
 
 static inline char *timenow() {
@@ -107,20 +86,5 @@ static inline char *timenow() {
     
     return buffer;
 }
-
-#ifdef __OBJC__
-
-static inline void objc_print(NSString *format, ...) {
-    AUTORELEASEPOOL_BEGIN
-    va_list args;
-    va_start(args, format);
-    NSString *logStr = [[NSString alloc] initWithFormat:format arguments:args];
-    fprintf(stderr, "%s", [logStr UTF8String]);
-    RELEASE(logStr);
-    va_end(args);
-    AUTORELEASEPOOL_END
-}
-
-#endif
 
 #endif
